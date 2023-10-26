a7bc214bc1ef2c30a44e4bf6fb0f1dbf9f1d2743
Adam Zulfikar <adam.zulfikar@nexsoft.co.id>
2023-10-26 13:15:26 +0700

    Add commit #1

---
+++ Makefile
@@ -0,0 +1,11 @@
+hello:
+	echo "hello"
+
+build:
+    go build -o bin/server main.go
+
+run: build
+    ./bin/server
+
+watch:
+    reflex -s -r '\.go$$' make run
--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/admzulfikar97/learn_gofiber_app_server/config"
+	"github.com/admzulfikar97/learn_gofiber_app_server/database"
+	"github.com/admzulfikar97/learn_gofiber_app_server/router"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	// app.Get("/", func(c *fiber.Ctx) {
-	// 	c.SendString("API GoFiber UP!!!")
-	// })
+	// Connect to DB
+	database.ConnectDB()
+
+	// Routing
 	var err error
 	app.Get("/", func(c *fiber.Ctx) error {
-		err = c.SendString("API GoFiber UP!!!")
+		err = c.SendString("")
+		p := config.Config("DB_PORT")
+		err = c.SendString(`--localhost:9077--
+		--API GoFiber UP!!!--`)
+		fmt.Println("PORT:", p)
 		return err
 	})
-	app.Get("/home", func(c *fiber.Ctx) error {
-		err = c.SendString("--API HOME SERVER--")
-		return err
-	})
+	// app.Get("/home", func(c *fiber.Ctx) error {
+	// 	err = c.SendString("--API HOME SERVER--")
+	// 	return err
+	// })
+	// app.Get("/health", func(c *fiber.Ctx) error {
+	// 	err = c.SendString("--Server localhost:9077 UP--")
+	// 	return err
+	// })
+	router.SetupRoutes(app)
 
 	app.Listen(":9077")
 }
